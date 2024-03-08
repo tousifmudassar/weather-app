cd647f3906da5176234b0ad0071b594b33c92c09
Mudassar Tousif <tousifmudassar@gmail.com>
2024-03-08 18:01:53 +0530

    Created Actions and Added Action

---
+++ src/services/actions/weatherActions.js
@@ -0,0 +1,19 @@
+// weatherActions.js
+export const FETCH_WEATHER_REQUEST = "FETCH_WEATHER_REQUEST";
+export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS";
+export const FETCH_WEATHER_FAILURE = "FETCH_WEATHER_FAILURE";
+
+export const fetchWeatherRequest = city => ({
+  type: FETCH_WEATHER_REQUEST,
+  payload: { city }
+});
+
+export const fetchWeatherSuccess = data => ({
+  type: FETCH_WEATHER_SUCCESS,
+  payload: { data }
+});
+
+export const fetchWeatherFailure = error => ({
+  type: FETCH_WEATHER_FAILURE,
+  payload: { error }
+});
